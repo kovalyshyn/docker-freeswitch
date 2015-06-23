64642f94f4171ca302dd4ab214a4a11bf4b0e420
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2015-06-23 20:49:14 +0300

    cleanup start script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$1" = 'freeswitch' ]; then
 	chown -R freeswitch:freeswitch /etc/freeswitch
 	chown -R freeswitch:freeswitch /var/{run,lib}/freeswitch
-	chown -R freeswitch:freeswitch /{logs,db,sounds,certs,scripts,recordings}
+	chown -R freeswitch:freeswitch /{db,sounds,certs,scripts,recordings}
 	
 	if [ -d /docker-entrypoint.d ]; then
 		for f in /docker-entrypoint.d/*.sh; do
@@ -14,9 +14,8 @@ if [ "$1" = 'freeswitch' ]; then
 	
 	ulimit -s 240
 	exec gosu freeswitch freeswitch -u freeswitch -g freeswitch -c \
-		-sounds /sounds -recordings /recordings -conf /etc/freeswitch \
-		-certs /certs -db /db -scripts /scripts \
-		-log /logs
+		-recordings /recordings -conf /etc/freeswitch \
+		-certs /certs -db /db -scripts /scripts	
 fi
 
 exec "$@"
