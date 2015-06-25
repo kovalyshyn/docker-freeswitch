4921a64b1ff9012e8f071b37d2681bbc8c47473c
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2015-06-25 17:54:02 +0300

    LOG in to /tmp

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$1" = 'freeswitch' ]; then
 	chown -R freeswitch:freeswitch /etc/freeswitch
 	chown -R freeswitch:freeswitch /var/{run,lib}/freeswitch
-	chown -R freeswitch:freeswitch /{db,certs,scripts,recordings}
+	chown -R freeswitch:freeswitch /{db,certs,scripts,recordings,tmp}
 	
 	if [ -d /docker-entrypoint.d ]; then
 		for f in /docker-entrypoint.d/*.sh; do
@@ -15,7 +15,7 @@ if [ "$1" = 'freeswitch' ]; then
 	ulimit -s 240
 	exec gosu freeswitch freeswitch -u freeswitch -g freeswitch -c \
 		-recordings /recordings -conf /etc/freeswitch \
-		-certs /certs -db /db -scripts /scripts	-log /var/logs
+		-certs /certs -db /db -scripts /scripts	-log /tmp
 fi
 
 exec "$@"
