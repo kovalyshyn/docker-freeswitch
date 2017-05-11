b6cf657bc2e4e46fe22001771591ecfaeadfd634
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2017-05-11 09:17:30 +0300

    update packages

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 if [ "$1" = 'freeswitch' ]; then
-	chown -R freeswitch:freeswitch /etc/freeswitch
-	chown -R freeswitch:freeswitch /var/{run,lib}/freeswitch
-	chown -R freeswitch:freeswitch /{db,certs,scripts,recordings,tmp}
-	
+    chown -R freeswitch:freeswitch /etc/freeswitch
+    chown -R freeswitch:freeswitch /var/{run,lib}/freeswitch
+    chown -R freeswitch:freeswitch /{db,certs,scripts,recordings,tmp}
+
 	if [ -d /docker-entrypoint.d ]; then
 		for f in /docker-entrypoint.d/*.sh; do
 			[ -f "$f" ] && . "$f"
